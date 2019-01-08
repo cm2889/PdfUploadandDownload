72a04eecdcc92fdd9d5019419deca7209e67df6f
Imam Hossain <imam2889@gmail.com>
2019-01-08 11:45:33 +0600

    How to upload pdf into the firebase and how to download the pdf file

---
+++ app/src/main/java/lab1/sdmgap/edu/pdfuploadanddownlad/Constants.java
@@ -0,0 +1,6 @@
+package lab1.sdmgap.edu.pdfuploadanddownlad;
+
+public class Constants {
+    public static final String STORAGE_PATH_UPLOADS = "uploads/";
+    public static final String DATABASE_PATH_UPLOADS = "uploads";
+}
---
+++ app/src/main/java/lab1/sdmgap/edu/pdfuploadanddownlad/ModelClass/Upload.java
@@ -0,0 +1,24 @@
+package lab1.sdmgap.edu.pdfuploadanddownlad.ModelClass;
+
+public class Upload {
+    public String name;
+    public String url;
+
+    // Default constructor required for calls to
+    // DataSnapshot.getValue(User.class)
+    public Upload() {
+    }
+
+    public Upload(String name, String url) {
+        this.name = name;
+        this.url = url;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+}
